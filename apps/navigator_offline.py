964c982717a9b80d4c02577ccd2db831c5673a9d
DangLamTung <danglamtung199@gmail.com>
2024-05-09 22:22:06 +0700

    Add files via upload
    Remove Geopy Geocoding, only  navigate  by coordinates

--- apps/navigator_offline.py
+++ apps/navigator_offline.py
@@ -8,7 +8,7 @@ ox.__version__
 from typing import Tuple, List
 from networkx.classes.multidigraph import MultiDiGraph
 
-
+import time 
 
 
 
@@ -26,12 +26,14 @@ def get_location_from_address(address: str) -> (float, float):
     Example:
         location_orig = get_location_from_address("Gare du Midi, Bruxelles")
     """
-    from geopy.geocoders import Nominatim
+    # from geopy.geocoders import Nominatim
 
-    locator = Nominatim(user_agent = "myapp")
-    location = locator.geocode(address)
-
-    return location.latitude, location.longitude
+    # locator = Nominatim(user_agent = "myapp")
+    # location = locator.geocode(address)
+    # print(address.replace("[","").split(","))
+    location = address.replace("[","").replace("]","").split(",")
+    print(location)
+    return float(location[0]), float(location[1])
 
 def get_graph(address_orig: str, address_dest: str) -> (MultiDiGraph, Tuple[float], Tuple[float]):
     """ 
@@ -89,6 +91,34 @@ def get_graph(address_orig: str, address_dest: str) -> (MultiDiGraph, Tuple[floa
 
 #     return graph, location_orig, location_dest
 
+def compare_find_shortest_path(graph: MultiDiGraph, location_orig: Tuple[float], location_dest: Tuple[float], optimizer: str) -> List[int]:
+    """
+    Find the shortest path between two points from the street graph
+    Args:
+        graph: street graph from OpenStreetMap
+        location_orig: departure coordinates
+        location_dest: arrival coordinates
+        optimizer: type of optimizer (Length or Time)
+    Returns:
+        route:
+    """
+
+    # find the nearest node to the departure and arrival location
+    # define origin and desination locations
+    TRAVEL_OPTIMIZER = ['Dijkstra', 'Bellman-Ford' ]
+    node_orig = ox.nearest_nodes(graph, location_orig[1],location_orig[0])
+    node_dest = ox.nearest_nodes(graph, location_dest[1],location_dest[0])
+    time_cal = []
+    routes = []
+    for method in TRAVEL_OPTIMIZER:
+        start = time.time()
+        route = nx.shortest_path(graph, node_orig, node_dest, weight='length', method=method.lower())
+        end = time.time()
+
+        routes.append(route)
+        time_cal.append(end  - start)
+        # print(end  - start)
+    return time_cal,routes
 
 def find_shortest_path(graph: MultiDiGraph, location_orig: Tuple[float], location_dest: Tuple[float], optimizer: str) -> List[int]:
     """
@@ -104,8 +134,11 @@ def find_shortest_path(graph: MultiDiGraph, location_orig: Tuple[float], locatio
 
     # find the nearest node to the departure and arrival location
     # define origin and desination locations
-
+    
     node_orig = ox.nearest_nodes(graph, location_orig[1],location_orig[0])
     node_dest = ox.nearest_nodes(graph, location_dest[1],location_dest[0])
+    start = time.time()
     route = nx.shortest_path(graph, node_orig, node_dest, weight='length', method=optimizer.lower())
+    end = time.time()
+    print(end  - start)
     return route
